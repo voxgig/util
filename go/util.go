480e70d7bc6ac49986a4400690deab85754e2c05
Richard Rodger <richard@ricebridge.com>
2026-07-20 15:26:06 +0100

    go: v0.1.4

--- go/util.go
+++ go/util.go
@@ -15,7 +15,7 @@ import (
 
 // Version is the released version of the Go module. It is rewritten by
 // `make publish-go V=x.y.z` to match the git tag (go/vx.y.z).
-const Version = "0.1.3"
+const Version = "0.1.4"
 
 // Camelify converts a kebab-case string (or slice of strings) to PascalCase.
 // Example: "foo-bar" => "FooBar"
